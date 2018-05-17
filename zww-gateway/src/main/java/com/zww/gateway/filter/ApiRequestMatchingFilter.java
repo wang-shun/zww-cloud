f5ff0926aff074c6f2fe2e86cbdbcd9dd04c1c6f
bruce <1050120599@qq.com>
2018-05-17 17:52:19 +0800

    提交

--- zww-gateway/src/main/java/com/zww/gateway/filter/ApiRequestMatchingFilter.java
+++ zww-gateway/src/main/java/com/zww/gateway/filter/ApiRequestMatchingFilter.java
@@ -11,7 +11,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 
